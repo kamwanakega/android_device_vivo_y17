ff7cb7ca5a7f77afa9a20c70a6371bd08bc35042
kamwanakega <kamwanakega254@gmail.com>
2025-07-30 23:28:18 +0300

    omni_VIVO-Y17.mk

--- omni_TECNO-KH6.mk
+++ omni_TECNO-KH6.mk
@@ -1,10 +1,8 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -12,18 +10,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from TECNO-KH6 device
-$(call inherit-product, device/tecno/TECNO-KH6/device.mk)
+# Inherit from PD1901 device
+$(call inherit-product, device/vivo/PD1901/device.mk)
 
-PRODUCT_DEVICE := TECNO-KH6
-PRODUCT_NAME := omni_TECNO-KH6
-PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO KH6
-PRODUCT_MANUFACTURER := tecno
+PRODUCT_DEVICE := PD1901
+PRODUCT_NAME := omni_PD1901
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := vivo PD1901
+PRODUCT_MANUFACTURER := vivo
 
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
+PRODUCT_GMS_CLIENTID_BASE := android-generic
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_kh6_h6513-user 12 SP1A.210812.016 353056 release-keys"
+    PRIVATE_BUILD_DESC="full_k65v1_64_bsp-user 9 P00610 compiler04082219 release-keys"
 
-BUILD_FINGERPRINT := TECNO/KH6-OP/TECNO-KH6:12/SP1A.210812.016/230613V1347:user/release-keys
+BUILD_FINGERPRINT := vivo/PD1901/PD1901:9/P00610/compiler04082219:user/release-keys
