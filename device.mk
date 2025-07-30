9455c8d94f44846dfa988c81946505f173c9b9d6
kamwanakega <kamwanakega254@gmail.com>
2025-07-30 23:25:21 +0300

    device.mk

--- device.mk
+++ device.mk
@@ -1,35 +1,13 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+LOCAL_PATH := device/vivo/PD1901
 
-LOCAL_PATH := device/tecno/TECNO-KH6
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6765
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6765 \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
     update_verifier \
     update_engine_sideload
