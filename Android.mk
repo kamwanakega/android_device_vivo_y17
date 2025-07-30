3f0957bf83cd8636d910c70d2b172760a13b340d
kamwanakega <kamwanakega254@gmail.com>
2025-07-30 23:17:56 +0300

    Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,10 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),TECNO-KH6)
+ifeq ($(TARGET_DEVICE),PD1901)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
